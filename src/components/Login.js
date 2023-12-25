c1e60bf767fcf2c9a65723eabeebbc4022e00ace
abhishek9125 <agarwal.a@mamaearth.in>
2023-12-25 22:31:06 +0530

    Added: Sign Up Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BG_URL } from '../utils/constants';
 import Header from './Header';
 
 function Login() {
+
+    const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm);
+    };
+
     return (
         <div>
             <Header />
@@ -13,29 +21,43 @@ function Login() {
                 onSubmit={(e) => e.preventDefault()}
                 className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
             >
-                 <h1 className="font-bold text-3xl py-4">
-                     Sign In
-                    {/* {isSignInForm ? "Sign In" : "Sign Up"} */}
+                <h1 className="font-bold text-3xl py-4">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
                 </h1>
+
+                {!isSignInForm && (
+                    <input
+                        // ref={name}
+                        type="text"
+                        placeholder="Full Name"
+                        className="p-4 my-4 w-full bg-gray-700"
+                    />
+                )}
+
                 <input
                     //   ref={email}
                     type="text"
                     placeholder="Email Address"
                     className="p-4 my-4 w-full bg-gray-700"
                 />
+
                 <input
                     //   ref={password}
                     type="password"
                     placeholder="Password"
                     className="p-4 my-4 w-full bg-gray-700"
                 />
-                <button
-                    className="p-4 my-6 bg-red-700 w-full rounded-lg"
-                    // onClick={handleButtonClick}
+
+                <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+
+                <button className="p-4 my-6 bg-red-700 w-full rounded-lg" // onClick={handleButtonClick}
                 >
-                    Sign In
-                    {/* {isSignInForm ? "Sign In" : "Sign Up"} */}
+                    {isSignInForm ? "Sign In" : "Sign Up"}
                 </button>
+
+                <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+                    {isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now."}
+                </p>
             </form>
         </div>
     )
