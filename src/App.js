f462d013fd5832a4d9ef5f50a5bb94b14fcfbbac
abhishek9125 <agarwal.a@mamaearth.in>
2023-12-25 21:53:16 +0530

    Added: Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="text-3xl font-bold text-green-800">
+      Building NetflixGPT
     </div>
   );
 }
